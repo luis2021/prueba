e354fdba33d936b1c14d42f0099cbc916f75a072
root <root@luis.168.1.5>
2018-11-19 18:18:27 -0400

    --amend

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ const menuBranding = document.querySelector('.menu .menu-branding');
 const menuNav = document.querySelector('.menu .menu-nav');
 const mainMenu = document.querySelector('header #main-menu');
 
+// para obtener el ancho de la la barra de scroll 
+const getScrollBarWidth = () => window.innerWidth - document.documentElement.getBoundingClientRect().width;
+// asignar ese valor a una variable css
+const cssScrollBarWidth = function(){
+    console.log("es: ",getScrollBarWidth());
+    document.documentElement.style.setProperty('--scrollbar',`${getScrollBarWidth()}px` );
+}
+window.addEventListener('load', cssScrollBarWidth);
+
 
 
 
