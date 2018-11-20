fd166053ed934dc35f6befdfb67a7dfb8d66c928
root <root@luis.168.1.5>
2018-11-19 23:24:04 -0400

    --amend

--- js/app.js
+++ js/app.js
@@ -4,49 +4,9 @@ const menuBtn = document.querySelector('div.menu-btn');
 const menuBranding = document.querySelector('.menu .menu-branding');
 const menuNav = document.querySelector('.menu .menu-nav');
 const mainMenu = document.querySelector('header #main-menu');
+const icons = document.querySelectorAll(".pagination .far");
 
-// para obtener el ancho de la la barra de scroll 
-const getScrollBarWidth = () => window.innerWidth - document.documentElement.getBoundingClientRect().width;
-// asignar ese valor a una variable css
-const cssScrollBarWidth = function(){
-    console.log("es: ",getScrollBarWidth());
-    document.documentElement.style.setProperty('--scrollbar',`${getScrollBarWidth()}px` );
-}
-window.addEventListener('load', cssScrollBarWidth);
-
-
-
-
-
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('mousemove', (e) => {
-  const sensitivity = 30;
-  const { offsetX, offsetY } = e;
-  const { offsetHeight, offsetWidth} = e.target;
-
-  const y = -((offsetX / offsetWidth) - 0.5) * sensitivity;
-  const x = ((offsetY / offsetHeight) - 0.5) * sensitivity;
-
-  btn.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(0) scale(1.2)`;
-});
-
-btn.addEventListener('mouseout', () => {
-  btn.style.transform = "scale(1.0) rotateX(0) rotateY(0)";
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// Evntos 
+// animaciones y eventos para el boton de menu 
 menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.toggle('close');
     menuBranding.classList.toggle('show');
@@ -54,9 +14,45 @@ menuBtn.addEventListener('click', ()=>{
     mainMenu.classList.toggle('show');
 });
 
+// transicion en la paginacion 
+icons[0].className = 'fas fa-circle';
+icons[0].style.color = '#DE4D3C'
 
 
 
+
+
+// animaciones para el boton de "hire me"
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mousemove', (e) => {
+    const sensitivity = 30;
+  const { offsetX, offsetY } = e;  
+  const { offsetHeight, offsetWidth} = e.target;
+
+  const y = -((offsetX / offsetWidth) - 0.5) * sensitivity;
+  const x = ((offsetY / offsetHeight) - 0.5) * sensitivity;
+
+  btn.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(0) scale(1.2)`;
+});  
+
+btn.addEventListener('mouseout', () => {
+    btn.style.transform = "scale(1.0) rotateX(0) rotateY(0)";
+});    
+
+
+
+
+
+
+// para obtener el ancho de la la barra de scroll para usarla en los entilos css
+const getScrollBarWidth = () => window.innerWidth - document.documentElement.getBoundingClientRect().width;
+// asignar ese valor a una variable css
+const cssScrollBarWidth = function(){
+    console.log("es: ",getScrollBarWidth());
+    document.documentElement.style.setProperty('--scrollbar',`${getScrollBarWidth()}px` );
+}
+window.addEventListener('load', cssScrollBarWidth);
 
 
 
